ed8fb7dc14430c3669f4488456e94bbdf1280ecc
tanveerns <tanveeralam229@gmail.com>
2022-09-04 18:42:59 +0530

    loader-scss-css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js'
 import addImage from './add-image.js'
+import Heading from './components/heading/heading.js'
 
+const Header = new Heading()
 const helloWorldButton = new HelloWorldButton()
+Header.render()
 helloWorldButton.render()
 addImage()
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-
 const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -20,11 +20,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.js$/,
@@ -40,6 +40,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new TerserPlugin()
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        })
     ]
   };
